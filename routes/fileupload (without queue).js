d9cfd87497f1cfe11c9ae693a805080c083a6573
Madhav Gupta <mgups2021@gmail.com>
2024-04-25 15:41:48 +0530

    changes

--- routes/fileupload (without queue).js
+++ routes/fileupload (without queue).js
@@ -3,17 +3,17 @@ const fileUpload = require("express-fileupload");
 const pdfParse = require("pdf-parse");
 const { Client } = require('@elastic/elasticsearch');
 const bodyParser = require("body-parser");
-const async = require('async');
 const client = new Client({
     node: 'https://197566baa3084a29a18f60154f5a9fe9.us-central1.gcp.cloud.es.io:443',
     auth: {
         apiKey: 'SUNVRUM0OEJuWlBCWTNsYWRPc086UVhPckY1a2lUWnV0bzlaZUprYVh3dw=='
     }
 });
-var cors = require('cors');
+const cors = require('cors');
 const app = express();
 const resp = client.info();
 const router = express.Router();
+
 app.use(bodyParser.json());
 app.use("/", express.static("public"));
 app.use(fileUpload());
@@ -23,7 +23,9 @@ async function index(data) {
     const result = await client.helpers.bulk({
         datasource: data,
         pipeline: "ent-search-generic-ingestion",
-        onDocument: (doc) => ({ index: { _index: 'search-pdf-docs' } }),
+        onDocument: (doc) => ({
+            index: { _index: 'search-pdf-docs' }
+        }),
     });
     console.log("Hi");
     console.log(result);
@@ -35,54 +37,32 @@ app.post("/list-files", async (req, res) => {
 });
 
 async function listFiles() {
-    const list = await client.search({
-        index: 'search-pdf-docs',
-    });
-    console.log(list.hits.hits)
-    return (list.hits.hits);
+    const list = await client.search({ index: 'search-pdf-docs' });
+    return list.hits.hits;
 }
 
-const uploadQueue = async.queue(async function (task, callback) {
-    const { req } = task;
-    console.log("called");
-    if (!req.files || !req.files.pdfFile) {
-        callback();
-        return;
-    }
-
-    const curr = { "domain": "", "title": "", "data": "", "link": "google.com" };
-    try {
-        const result = await pdfParse(req.files.pdfFile);
-        curr.title = req.files.pdfFile.name;
-        curr.data = (JSON.stringify(result.text)).replaceAll(/\['"\]/g, '').replaceAll(/\\\\n/g, ' ');
-        curr.domain = req.body.domain;
-        const datarow = [curr];
-        await index(datarow);
-        callback();
-    } catch (err) {
-        console.error("Error parsing PDF:", err);
-        callback(err);
-    }
-}, 1); // Set concurrency to 1 to process files sequentially
-
 app.post("/extract-text", (req, res) => {
-    uploadQueue.push({ req }, (err) => {
-        if (err) {
-            console.error("Error processing file:", err);
-            res.status(500).send("Error processing file");
-        } else {
-            res.status(200).send("File processed successfully");
-        }
+    console.log("called");
+    if (!req.files && !req.files.pdfFile) {
+        res.status(400);
+        res.end();
+    }
+    const datarow = [];
+    const curr = { "domain": "", "title": "", "data": "", "link": "google.com" };
+    pdfParse(req.files.pdfFile).then(result => {
+        curr.title = req.files.pdfFile.name;
+        curr.data = (JSON.stringify(result.text)).replaceAll(/['"]/g, '').replaceAll(/\\n/g, ' ');
+        curr.domain = req.body.domain;
+        datarow.push(curr);
+        index(datarow);
+        res.send(curr.data);
     });
 });
 
 app.post("/delete-file", async (req, res) => {
     const fileId = req.body.fileId;
     try {
-        const { body: result } = await client.delete({
-            index: 'search-pdf-docs',
-            id: fileId
-        });
+        const { body: result } = await client.delete({ index: 'search-pdf-docs', id: fileId });
         console.log("Document deleted:", result);
         res.json({ success: true });
     } catch (error) {
@@ -96,7 +76,6 @@ app.post("/delete-all", async (req, res) => {
 });
 
 console.log("hello");
-const list = [];
 
 async function deleteall() {
     await client.indices.delete({ index: 'search-pdf-docs' });
