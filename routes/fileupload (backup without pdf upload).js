36c3d2a84b6c13ce828bc280d8fad49c1a767e90
Madhav Gupta <mgups2021@gmail.com>
2024-04-27 16:23:04 +0530

    PDF uploading on Google Cloud

--- routes/fileupload (backup without pdf upload).js
+++ routes/fileupload (backup without pdf upload).js
@@ -21,33 +21,6 @@ app.use(bodyParser.json());
 app.use("/", express.static("public"));
 app.use(fileUpload());
 
-console.log("Started Upload Server");
-
-const { Storage } = require('@google-cloud/storage');
-require('dotenv').config();
-
-const projectId = process.env.PROJECT_ID;
-const keyFilename = process.env.KEYFILENAME;
-const storage = new Storage({ projectId, keyFilename });
-
-async function uploadPDF(file) {
-    try {
-        const bucketName = process.env.BUCKET_NAME;
-        const bucket = storage.bucket(bucketName);
-        const fileExtension = file.name.split('.').pop();
-        const fileOutputName = `${Date.now()}.${fileExtension}`;
-        const filePath = "./Dummy PDFs/" + file.name
-        const ret = await bucket.upload(filePath, { destination: fileOutputName });
-        console.log(`File uploaded to ${ret[0].metadata.mediaLink}`);
-        const link = "https://storage.googleapis.com/" + process.env.BUCKET_NAME + "/" + fileOutputName;
-        console.log(link);
-        return link;
-    } catch (error) {
-        console.error('Error uploading file:', error);
-        throw error;
-    }
-}
-
 async function index(data) {
     console.log(data);
     const result = await client.helpers.bulk({
@@ -59,10 +32,48 @@ async function index(data) {
     console.log(result);
 }
 
-const uploadQueue = async.queue(async function (task) {
+app.post("/list-files", async (req, res) => {
+    const ourList = await listFiles();
+    res.send(ourList);
+});
+app.post("/list-all-files", async (req, res) => {
+    const ourList = await listAllFiles();
+    res.send(ourList);
+});
+
+async function listFiles() {
+    const list = await client.search({
+        index: 'search-pdf-docs',
+        "size": 15,
+        "query": {
+            "match_all": {}
+        }
+
+    });
+    console.log(list.hits.hits)
+    console.log("SIZE IS" + (list.hits.hits).length)
+    return (list.hits.hits);
+}
+
+async function listAllFiles() {
+    const list = await client.search({
+        index: 'search-pdf-docs',
+        "size": 300,
+        "query": {
+            "match_all": {}
+        }
+
+    });
+    console.log(list.hits.hits)
+    console.log("SIZE IS" + (list.hits.hits).length)
+    return (list.hits.hits);
+}
+
+const uploadQueue = async.queue(async function (task, callback) {
     const { req } = task;
     console.log("called");
     if (!req.files || !req.files.pdfFile) {
+        callback();
         return;
     }
 
@@ -72,16 +83,16 @@ const uploadQueue = async.queue(async function (task) {
         curr.title = req.files.pdfFile.name;
         curr.data = (JSON.stringify(result.text)).replaceAll(/\['"\]/g, '').replaceAll(/\\\\n/g, ' ');
         curr.domain = req.body.domain;
-        curr.link = await uploadPDF(req.files.pdfFile); // Call the uploadPDF function here
         const datarow = [curr];
         await index(datarow);
+        callback();
     } catch (err) {
         console.error("Error parsing PDF:", err);
+        callback(err);
     }
 }, 1);
 
 app.post("/extract-text", (req, res) => {
-    console.log(req.files.pdfFile)
     uploadQueue.push({ req }, (err) => {
         if (err) {
             console.error("Error processing file:", err);
@@ -91,12 +102,6 @@ app.post("/extract-text", (req, res) => {
         }
     });
 });
-
-
-async function deleteall() {
-    await client.indices.delete({ index: 'search-pdf-docs' });
-    await client.indices.create({ index: 'search-pdf-docs' });
-}
 
 app.post("/delete-file", async (req, res) => {
     const fileId = req.body.fileId;
@@ -116,41 +121,14 @@ app.post("/delete-file", async (req, res) => {
 app.post("/delete-all", async (req, res) => {
     deleteall();
 });
-app.post("/list-files", async (req, res) => {
-    const ourList = await listFiles();
-    res.send(ourList);
-});
-app.post("/list-all-files", async (req, res) => {
-    const ourList = await listAllFiles();
-    res.send(ourList);
-});
 
-async function listFiles() {
-    const list = await client.search({
-        index: 'search-pdf-docs',
-        "size": 15,
-        "query": {
-            "match_all": {}
-        }
+console.log("hello");
+const list = [];
 
-    });
-    // console.log(list.hits.hits)
-    console.log("Retuned " + (list.hits.hits).length + " files for preview")
-    return (list.hits.hits);
+async function deleteall() {
+    await client.indices.delete({ index: 'search-pdf-docs' });
+    await client.indices.create({ index: 'search-pdf-docs' });
 }
 
-async function listAllFiles() {
-    const list = await client.search({
-        index: 'search-pdf-docs',
-        "size": 300,
-        "query": {
-            "match_all": {}
-        }
-
-    });
-    console.log(list.hits.hits)
-    console.log("SIZE IS" + (list.hits.hits).length)
-    return (list.hits.hits);
-}
 app.listen(9001);
 module.exports = router;
